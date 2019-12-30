2a246d5e467a9cc524c69825f7c3908209c0e15d
rowland <rowland.lan@163.com>
2019-12-30 10:39:52 +0800

    adjust

--- rc/ezlib.sh
+++ rc/ezlib.sh
@@ -22,4 +22,38 @@ function _update_ezjp() {
     cd -
 }
 
-
+function _gen_expect() {
+    if [ ! -f $EZJP_HOME/h/$1 ]; then
+        echo "No such file"
+        return 1
+    fi
+    local i=0
+    printf "spawn ">$EZJP_HOME/tmp/a.ff
+    cat $EZJP_HOME/h/$1|sed 's/#.*//'|sed -n '/./p'|while read line||[ -n "$line" ]; do
+        local host=`echo ${line}|awk '{print $1}'`
+        local port=`echo ${line}|awk '{print $2}'`
+        local user=`echo ${line}|awk '{print $3}'`
+        local pass=`echo ${line}|awk '{print $4}'`
+        local cmd="ssh $user@$host -p$port"
+        if [[ $i>0 ]]; then
+            cmd="send \"ssh $user@$host -p$port\r\""
+        fi
+        ((i++))
+        cat<<EOF>>$EZJP_HOME/tmp/a.ff
+$cmd
+expect {
+  "*password:*" {
+    send "$pass\r"
+  }
+  "*yes/no*" {
+    send "yes\r"
+    expect "password:*"
+    send "$pass\r"
+  }
+}
+expect "*$user*"
+EOF
+    done
+    cat $EZJP_HOME/tmp/a.ff|sed '$d'>$EZJP_HOME/tmp/ezexp.exp
+    echo interact>>$EZJP_HOME/tmp/ezexp.exp
+}
